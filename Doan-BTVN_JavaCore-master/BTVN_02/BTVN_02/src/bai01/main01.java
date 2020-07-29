048ce7b560919919f02d7e6a27483d049fd485e8
Nam Doan <doanduynam2000@gmail.com>
2020-07-29 17:55:54 +0700

    del

--- Doan-BTVN_JavaCore-master/BTVN_02/BTVN_02/src/bai01/main01.java
+++
@@ -1,9 +0,0 @@
-package bai01;
-
-public class main01 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, this is my first application.");
-	}
-
-}
--- Doan-BTVN_JavaCore-master/BTVN_04/HomeWork_04/src/Bai_03/RunMain.java
+++
@@ -1,9 +0,0 @@
-package Bai_03;
-
-public class RunMain {
-	public static void main(String[] args) {
-		JavaClass Java_6 = new JavaClass();
-		Java_6.InputClassInfo();
-		Java_6.ShowClassInfo();
-	}
-}
